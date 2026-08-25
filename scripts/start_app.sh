c0a9d02a4aee06ac52f7e4a900d3d4f51217ce4a
apiesamson <108661479+Apiesamson@users.noreply.github.com>
2026-08-25 10:31:37 +0200

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["35.180.135.50"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["35.180.205.209"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
