228788821c8c89a71ab3016dacb7038e910de904
apiesamson <108661479+Apiesamson@users.noreply.github.com>
2026-08-25 10:02:38 +0200

    Vérifie que la page d'accueil réponds

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class HomePageTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_page_status_code(self):
+        """Vérifie que la page d'accueil répond avec succès (code 200)"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
